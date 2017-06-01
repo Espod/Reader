11669be2a29ca18dfe2af884e3bdf474187e33be
espod <zhangming9797@gmail.com>
2017-06-01 10:15:38 +0800

    Initial commit

---
+++ src/main/java/com/espod/com/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.espod.com;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ReaderApplication.class);
+	}
+
+}
